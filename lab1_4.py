e6892d1541d03921ea875b65e9e76afd0175648c
Mason-Wescott <mjwescot@calpoly.edu>
2026-01-14 18:18:14 -0800

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,3 +1,14 @@
-calculate_average(num1,num2,num3)
-add_tax(bill_total)
-greet_user(name)
+num1 = 1
+num2 = 2
+num3 = 3
+numnum = 3
+bill = 10
+salestax = 1.1
+def calculate_average((num1+num2+num3)/numnum)
+def add_tax((bill * salestax)
+def greet_user(input("What is your name? "))
+print(calculate_average)
+print(add_tax)
+greet_user
+print("Hello " + greet_user)
+
